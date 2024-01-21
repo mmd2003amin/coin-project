be2c36586b86065e8715c0c44a2807c96c4357ac
Amin Basati <mohamadamin386mm@gmail.com>
2024-01-21 10:49:56 +0330

    Add the banner section to the project

--- coin-project/src/components/App.jsx
+++ coin-project/src/components/App.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Menu from "./Menu";
+import Banner from './Banner';
 
 const App = () => {
   const [ showMenu , setShowMenu ] = useState(false);
 
   return (
-    <div className='bg-stone-950 h-[100vh]'>
+    <div className='bg-stone-950 min-h-[100vh] h-full'>
       <Navbar showMenu={showMenu} setShowMenu={setShowMenu} />
       <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
+      <Banner />
     </div>
   );
 };
